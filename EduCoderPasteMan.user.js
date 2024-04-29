5265a283deaeb7c155830b65a75156b741feddec
Lydia <740297616@qq.com>
2024-04-29 10:38:22 +0800

    Update some beauty statement.

--- EduCoderPasteMan.user.js
+++ EduCoderPasteMan.user.js
@@ -34,8 +34,19 @@
 
     }
 
-    const message = "脚本已运行。你现在可以自由地复制粘贴了。"
-    const insetAlertHTML = '<div id="alersContainer" class="ant-message ant-message-top css-13xy8lc" style="left: 50%; transform: translateX(-50%); top: 8px;"><div class="ant-message-notice ant-message-notice-warning css-13xy8lc"><div class="ant-message-notice-content"><div class="ant-message-custom-content ant-message-warning"><span role="img" aria-label="exclamation-circle" class="anticon anticon-exclamation-circle"><svg viewBox="64 64 896 896" focusable="false" data-icon="exclamation-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path></svg></span><span>'+ message +'</span></div></div></div></div>';
+    // 一些可爱的msg
+    const messages = [
+        "脚本已运行。你现在可以自由地复制粘贴了。",
+        "什么垃圾edocoder，monaco写的一坨💩",
+        "已破解头歌的复制粘贴，Enjoy It ！",
+        "FBI Warning⚠️⚠️⚠️! Already F***ed Educoder's ass! Damn!",
+    ]
+
+    // ramdom取index
+    const randomIndex = Math.floor(Math.random() * messages.length)
+    const message = messages[randomIndex]   // 拿出一些美妙语言
+
+    const insetAlertHTML = '<div id="alersContainer" class="ant-message ant-message-top css-13xy8lc" style="margin-top: 20%; left: 50%; transform: translateX(-50%); top: 8px;"><div class="ant-message-notice ant-message-notice-warning css-13xy8lc"><div class="ant-message-notice-content"><div class="ant-message-custom-content ant-message-warning"><span role="img" aria-label="exclamation-circle" class="anticon anticon-exclamation-circle"><svg viewBox="64 64 896 896" focusable="false" data-icon="exclamation-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path></svg></span><span>'+ message +'</span></div></div></div></div>';
 
     var tempDiv = document.createElement('div');
     tempDiv.innerHTML = insetAlertHTML;
