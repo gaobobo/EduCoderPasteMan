153fd245490c625340f3b068e120a164ed149b7c
Shibo Gao <happygaoshibo@outlook.com>
2024-04-30 09:27:05 +0800

    fix wrong version of header

--- EduCoderPasteMan.user.js
+++ EduCoderPasteMan.user.js
@@ -5,7 +5,7 @@
 // @description  Copy and paste for unlocking the level of the EduCoder. This script is only enabled on the public network EduCoder by default. If you want to enable it on the private network/school network EduCoder, please edit the script manually. Editing instructions are included in the script.
 // @namespace    https://bbs.tampermonkey.net.cn/
 // @source       https://github.com/gaobobo/EduCoderPasteMan
-// @version      1.0.0
+// @version      1.0.2
 // @author       Gao Shibo
 // @match        https://www.educoder.net/*
 // @match        https://vpn.zcst.edu.cn/*
